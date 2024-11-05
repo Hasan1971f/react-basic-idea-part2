9a22935e426cc50365b1253594c7f9ef4d3f3387
Hasan <mdhasanurrahmanhasan101@gmail.com>
2024-11-05 13:58:10 +0600

    added a useState and UseEffect basic idea with data load and data fetching

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+
+import './App.css'
+import Counter from './Counter'
+import Team from './Team'
+import Users from './Users'
+
+function App() {
+  function handleClick(){
+    alert('button clicked')
+  }
+
+  const handleClick2 =()=>{
+    alert('button 2 clicked')
+
+  }
+
+
+  const addToFive=(num)=>{
+    alert( num * 6)
+  }
+
+
+return (
+    <>
+      <h3>Rect core concepts 2</h3>
+      <Users></Users>
+      <Team></Team>
+      <Counter></Counter>
+      
+
+      <button onClick={ handleClick}>Click Me</button>
+      <button onClick={ handleClick2}>Click 2</button>
+
+      <button onClick={()=>{alert('thirrd click')}}>Third</button>
+
+
+      <button onClick={()=>{ addToFive(6)}}>Four</button>
+
+
+
+    </>
+  )
+}
+
+export default App
